10e740ed2deecab267cc597aef48d1e970829dc3
noah-sheppard <noah.sheppard@mymail.champlain.edu>
2025-04-28 18:23:39 -0400

    Added Cert of Authenticity

--- server.py
+++ server.py
@@ -1,3 +1,20 @@
+"""Final Project, Threaded TCP Chat.
+
+Author: Noah Sheppard
+Class: CSI-275-01
+Assignment: Final Project
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
+
 import socket
 import threading
 import json
@@ -6,7 +23,7 @@ import logging
 import sys
 import time
 
-# Basic logging setup
+# logging setup
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(threadName)s] %(levelname)s - %(message)s')
 
 # --- Protocol Helpers (Essential) ---
